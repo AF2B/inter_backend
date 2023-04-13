781011edd79625d9352572b7c5c32e306d9dd4fc
andre.borba <dev_andre.filipe@outlook.com>
2023-04-13 09:50:22 -0300

    feat(seed): a simple injection of user data in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+50.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number,
+    password: Faker::Internet.password
+  )
+end
