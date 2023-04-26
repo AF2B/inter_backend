c4e4cd7c9efcefc62e5e3189f234f5683e18f114
andre.borba <dev_andre.filipe@outlook.com>
2023-04-26 20:00:06 -0300

    feat(schema): restarted

--- db/schema.rb
+++ db/schema.rb
@@ -10,83 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_194456) do
-  create_table "donations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "quantity"
-    t.date "date"
-    t.bigint "organization_id", null: false
-    t.bigint "food_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["food_id"], name: "index_donations_on_food_id"
-    t.index ["organization_id"], name: "index_donations_on_organization_id"
-  end
-
-  create_table "feedbacks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_feedbacks_on_user_id"
-  end
-
-  create_table "foods", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "category"
-    t.text "description"
-    t.date "validity"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
-  end
-
-  create_table "organizations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_04_26_194511) do
+  create_table "ongs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "phone"
     t.string "address"
-    t.string "document"
+    t.string "password"
+    t.string "cnpj"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "cnpj"
   end
 
-  create_table "photos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "description"
-    t.string "image"
-    t.bigint "donation_id", null: false
+  create_table "outofstocks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "ong_id", null: false
+    t.string "product"
+    t.integer "quantity"
+    t.decimal "weight", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["donation_id"], name: "index_photos_on_donation_id"
+    t.index ["ong_id"], name: "index_outofstocks_on_ong_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password"
-    t.boolean "is_donors"
     t.string "address"
-  end
-
-  create_table "volunteers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "phone"
-    t.text "skills"
-    t.text "experience"
-    t.string "available_time"
-    t.string "location"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "donations", "foods"
-  add_foreign_key "donations", "organizations"
-  add_foreign_key "feedbacks", "users"
-  add_foreign_key "foods", "users"
-  add_foreign_key "photos", "donations"
+  add_foreign_key "outofstocks", "ongs"
 end
