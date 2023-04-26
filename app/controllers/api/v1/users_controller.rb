7914a6b220821565af5265d614e51d9384739c0b
andre.borba <dev_andre.filipe@outlook.com>
2023-04-26 19:58:55 -0300

    feat(controller): users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,73 +8,17 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    unless required_params_present?
-      return render json: { message: 'Required params are missing!' }, status: :bad_request
-    end
-
-    user = User.new(user_params)
+    user = User.new(users_params)
 
     if user.save
-      render json: { user: {id: user.id}, message: 'User created succefully!' }, status: :created
+      render json: { userId: user.id }, status: :created
     else
-      render json: { message: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { message: user.errors.full_messages }, status: :bad_request
     end
-  end
-
-  def show
-    id = params[:id] if params[:id].present? && params[:id].to_i.positive?
-
-    unless id && User.exists?(id)
-      return render json: { message: 'User not found!' }, status: :not_found
-    end
-
-    user = User.find(id)
-    render json: { data: user_json_uniq_user_structure(user) }, status: :ok
   end
 
   private
-  def user_json_structure(data)
-    data.each do |item|
-      {
-        data: {
-          id: item.id,
-          name: item.name,
-          email: item.email,
-          phone: item.phone,
-          address: item.address,
-          created_at: item.created_at,
-          updated_at: item.updated_at
-        }
-      }
-    end
-  end
-
-  def user_json_uniq_user_structure(user)
-    data = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      phone: user.phone,
-      address: user.address,
-      created_at: user.created_at,
-      updated_at: user.updated_at
-    }
-    data
-  end
-
-  def user_params
-    params.require(:user).permit(
-      :name,
-      :email,
-      :phone,
-      :address,
-      :password,
-      :password_confirmation
-    )
-  end
-
-  def required_params_present?
-    required_params = [:name, :email, :phone, :address, :password, :password_confirmation]
-    required_params.all? { |param| user_params[param].present? }
+  def users_params
+    params.permit(:name, :email, :phone, :address, :password)
   end
 end
