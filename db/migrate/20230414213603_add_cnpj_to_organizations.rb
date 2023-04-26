5e93ee7e712c4033e7033a62bd0f4fcd89274206
andre.borba <dev_andre.filipe@outlook.com>
2023-04-26 19:57:17 -0300

    chore: restarted

--- db/migrate/20230414213603_add_cnpj_to_organizations.rb
+++
@@ -1,5 +0,0 @@
-class AddCnpjToOrganizations < ActiveRecord::Migration[7.0]
-  def change
-    add_column :organizations, :cnpj, :string
-  end
-end
--- db/migrate/20230414220806_add_is_active_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIsActiveToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :is_donors, :boolean
-  end
-end
